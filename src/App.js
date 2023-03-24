c3c56362350466952f7aa7602374b72b146f831a
kishanurankar <kishanurankar@gmail.com>
2023-03-24 22:07:21 +0530

    change url

--- src/App.js
+++ src/App.js
@@ -6,35 +6,30 @@ function App() {
   const [book, setBook] = useState([]);
 
   const createBook = (title) => {
-    
-    const updatedTitle = [ ...book, title];
+    const updatedTitle = [...book, title];
     setBook(updatedTitle);
     console.log("Book name is", title);
-    
   };
 
   const removeBookAtIndex = (indexToRemove) => {
-    const updatedBook = book.filter((books,index) => {
+    const updatedBook = book.filter((books, index) => {
       return indexToRemove !== index;
     });
     setBook(updatedBook);
-  }
+  };
 
-  const renderedBook = book.map((books, i)=> {
-    return<li key={i}>{books}</li>
-  })
+  const renderedBook = book.map((books, i) => {
+    return <li key={i}>{books}</li>;
+  });
 
   return (
     <div>
-    <h3>Adding Books</h3>
-      <BookCreate onCreate={createBook}/>
-      
-      <ul>
-        
-        {renderedBook}
-      </ul>
+      <h3>Adding Books</h3>
+      <BookCreate onCreate={createBook} />
+
+      <ul>{renderedBook}</ul>
       <hr />
-      <RemoveForm onSubmit={removeBookAtIndex} max={book.length}/>
+      <RemoveForm onSubmit={removeBookAtIndex} max={book.length} />
     </div>
   );
 }
