5a6188569fe854202850cd4f4a7bb85744bee485
kishanurankar <kishanurankar@gmail.com>
2023-03-24 21:26:18 +0530

    update

--- src/components/RemoveForm.js
+++ src/components/RemoveForm.js
@@ -1,14 +1,24 @@
-function RemoveForm({ max }) {
+import { useState } from "react";
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    }
+function RemoveForm({onSubmit, max }) {
+  const [n, setN] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(n);
+  };
+
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Remove At Index</label>
-        <input type="number" min={0} max={max > 0 ? max - 1 : 0} />
+        <input
+          type="number"
+          min={0}
+          max={max > 0 ? max - 1 : 0}
+          onChange={(e) => setN(parseInt(e.target.value))}
+        />
         <button>Submit</button>
       </div>
     </form>
